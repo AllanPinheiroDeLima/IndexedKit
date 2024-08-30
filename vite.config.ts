4624b827b02cfd65e6f5e3776843b9d883f04430
Allan Lima <allan.lima@redspark.io>
2024-08-30 19:21:23 -0300

    build add processo de build

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vitest/config'
+import { loadEnv } from "vite";
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'node:path';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'), // ponto de entrada da sua biblioteca
+      name: 'SimpleDataStore', // nome da biblioteca
+      fileName: (format) => `simple-data-store.${format}.js`, // nome do arquivo de saída
+      formats: ['es', 'umd'], // formatos de saída
+    },
+    // rollupOptions: {
+    //   // Certifique-se de que você não inclua as dependências na build da biblioteca
+    //   external: ['uuid'], // adicione outras dependências que não deseja incluir
+    //   output: {
+    //     globals: {
+    //       uuid: 'uuid', // nome global para a dependência externa
+    //     },
+    //   },
+    // },
+  },
+  plugins: [tsconfigPaths()],
+  test: {
+    name: 'happy-dom',
+    root: './tests',
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./tests/setup.ts'],
+    env: loadEnv('', process.cwd(), ''),
+    reporters: ["verbose"],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './coverage',
+      exclude: ['**/node_modules/**'],
+      provider: 'istanbul' // or 'v8'
+    },
+  },
+})
